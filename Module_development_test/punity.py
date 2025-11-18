e059bb2ac5a99a67a855ed0a2937060fcb336042
IHaveNooIdeaWhatIamDoing <domenik08sabitzer@gmail.com>
2025-11-18 18:12:23 +0100

    Rendering and scalling for the objects

--- Module_development_test/punity.py
+++ Module_development_test/punity.py
@@ -1,6 +1,7 @@
-from pydoc import sort_attributes
+
 
 import pygame
+from pygame.transform import scale
 
 
 ### CLASSES ###
@@ -8,12 +9,8 @@ import pygame
 # GameObject Class
 class GameObject:
     def __init__(self, transform: "Transform2D", sprite: "Sprite" = None):
-        if not isinstance(transform, Transform2D):
-            raise TypeError("transform must be of type transform2D")
-        if not isinstance(sprite, Sprite):
-            raise TypeError("sprite must be of type Sprite")
 
-        print("GameObejct has been created")
+        print("GameObject has been created")
 
         self.transform = transform
         self.sprite = sprite
@@ -30,7 +27,7 @@ class Transform2D:
 
 # Sprite Renderer
 class Sprite:
-    def __init__(self, image_path : str, layer : int = 0, convertion : bool = False ):
+    def __init__(self, image_path : str,  layer : int = 0, convertion : bool = False ):
         self.image_path = image_path
         self.layer = layer
         self.convertion = convertion
@@ -40,21 +37,37 @@ class Sprite:
                 self.surface = pygame.image.load(self.image_path).convert_alpha()
             else:
                 self.surface = pygame.image.load(self.image_path)
-
-
+        self.original_surface = self.surface
 
 
 # Object in a Scene List
 objects_in_scene = [] # List of all GameObject that exist in the Scene
 
 # instantiate a new Gameobject
-def instantiate(game_object : GameObject):
-     objects_in_scene.append(game_object)
+def add_to_rendering(game_object : GameObject):
+    """Adds a GameObject to the rendering que. GameObject must have Sprite-Class"""
+    objects_in_scene.append(game_object)
 
-def render_objects(objects_in_scene : list, screen):
-    render_que_list = sorted(objects_in_scene, key=lambda obj: obj.sprite.layer)
-    for object in render_que_list:
 
+def render_objects(objects : list, screen: pygame.surface.Surface):
+    """Renders all the objects in the scene"""
+    render_que_list = sorted(objects, key=lambda obj: obj.sprite.layer)
+    for obj in render_que_list:
+        # Scale the object
+        obj = scale_obj(obj)
+        # Draw the Object on the screen
+        screen.blit(obj.sprite.surface, (obj.transform.x_position, obj.transform.y_position))
+
+def scale_obj(obj: GameObject):
+    """Scales the Object by the scale of the transform-scale variables"""
+    if obj.sprite:
+        width = obj.sprite.original_surface.get_width()* obj.transform.x_scale
+        height = obj.sprite.original_surface.get_height()* obj.transform.y_scale
+        new_surface = pygame.transform.scale(obj.sprite.surface, (width, height))
+        obj.sprite.surface = new_surface
+        return obj
+    else:
+        raise TypeError("tries to scale an object that has no sprite")
 
 
 
@@ -64,7 +77,7 @@ def render_objects(objects_in_scene : list, screen):
 
 def test_object_in_scene():
     player = GameObject(Transform2D(0, 0), Sprite("assets/player.png"))
-    instantiate(player)
+    add_to_rendering(player)
     print(f"First objects in a scene: {objects_in_scene}")
     print_object_position(player, "player")
     player.transform = Transform2D(10, 10)
@@ -82,7 +95,17 @@ def test_transform():
 def print_object_position(obj : GameObject, name: str):
     print(f"{name} | pos: {obj.transform.x_position, obj.transform.y_position} ")
 
+def main(screen: pygame.surface.Surface):
+    rendering(screen)
+
+def rendering(screen: pygame.surface.Surface, background_color: pygame.color.Color = (0,0,0)):
+    """Renders everything"""
+    screen.fill(background_color)
+    render_objects(objects_in_scene, screen)
+    pygame.display.update()
+
+
+
 ### Start Test
-test_transform()
 
 ### End Test
--- Module_development_test/testGame.py
+++ Module_development_test/testGame.py
@@ -1,12 +1,29 @@
 import pygame
+import punity
+from Module_development_test.punity import Transform2D, Sprite
 
 pygame.init()
 screen = pygame.display.set_mode((1080, 720))
 running = True
 
+# Pictures
+player_img_path = "C:/Domenik/Programming2/python-Projects/PyGameTest/Module_development_test/test_pictures/ball.png"
+
+loaded_img = pygame.image.load(player_img_path)
+# Create a GameObject
+player = punity.GameObject(
+                Transform2D(20,30, x_scale=0.2, y_scale=0.2),
+                Sprite(image_path=player_img_path,layer=1),
+                )
+punity.add_to_rendering(player)
+
+
+
 while running:
 
-
+    # Testing
+    punity.main(screen)
+    #screen.blit(loaded_img, (10, 10))
 
     # Events loop
     for event in pygame.event.get():
